c2dd4bec5ffeb43fd7aa1b4040e3bfab8fd78a7d
DigitOtter <digitotter@protonmail.com>
2023-12-11 09:47:30 +0100

    Add example function to retrieve mesh UV coordinates at ray intersection point.

--- main.gd
+++ main.gd
@@ -17,6 +17,31 @@ var print_time: float = 0.0
 @onready
 var indicator: MeshInstance3D = load("res://dot.tscn").instantiate()
 
+func _get_uv_coordinates(mesh_triangle_point: MeshTrianglePoint) -> Vector2:
+	# Gets uv coordinates of a mesh_triangle_point
+	# Note: Does not follow skeleton/blendshape transforms! The function should only be used with an up-to-date mesh_triangle_point
+	if not mesh_triangle_point.mesh_instance:
+		return Vector2.INF
+	
+	var triangle_vertices: PackedVector3Array = self.triangle_ray_select.get_triangle_vertices(mesh_triangle_point)
+	if triangle_vertices.size() != 3:
+		return Vector2.INF
+	
+	var barycentric_point: Vector3 = \
+		Geometry3D.get_triangle_barycentric_coords(mesh_triangle_point.point_on_triangle, triangle_vertices[0], triangle_vertices[1], triangle_vertices[2])
+	
+	# Get mesh data. If this function is called often, consider only calling surface_get_arrays() once and storing the data somewhere
+	var mesh_arrays: Array = mesh_triangle_point.mesh_instance.mesh.surface_get_arrays(mesh_triangle_point.surface_id)
+	var uv_data: PackedVector2Array =  mesh_arrays[Mesh.ARRAY_TEX_UV]
+	
+	# Get UV position using barycentric coordinates
+	var uv_pos: Vector2 = \
+	uv_data[mesh_triangle_point.vertex_ids[0]] * barycentric_point[0] + \
+	uv_data[mesh_triangle_point.vertex_ids[1]] * barycentric_point[1] + \
+	uv_data[mesh_triangle_point.vertex_ids[2]] * barycentric_point[2]
+	
+	return uv_pos
+
 func _move_cat_root_skeleton(time):
 	var skeleton: Skeleton3D = $"chibi_cat/Chibi Cat/Skeleton3D"
 	var bone_id: int = skeleton.find_bone("root")
@@ -43,6 +68,9 @@ func _input(event):
 		if self.mesh_triangle_point.mesh_instance:
 			# Create transform for point_on_triangle from triangle vertices
 			self.triangle_transform  = self.triangle_ray_select.get_triangle_transform_msi(self.mesh_triangle_point, Transform3D(Basis(), self.mesh_triangle_point.point_on_triangle))
+			
+			var uv_coords: Vector2 = self._get_uv_coordinates(self.mesh_triangle_point)
+			print("UV Coordinate: ", uv_coords)
 
 func _process(delta):
 	# Every second, print the position of last point_on_triangle
